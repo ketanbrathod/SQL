fe81084417604324b6a03a4ee5795de89d227a7e
Ketan Rathod <40425806+ketanbrathod@users.noreply.github.com>
2025-07-23 15:23:04 +0530

    Delete SQL-23-07-2025.sql

--- SQL-23-07-2025.sql
+++
@@ -1,7 +0,0 @@
-
-1. Find out how many employees are in each department.
-2. Calculate the average salary for employees in each department.
-3. Find the total quantity sold for each distinct product.
-4. Maximum Price of a Product in Each Region
-5. Minimum and Maximum Salary per Department
-6. Total Sales Value per Region
